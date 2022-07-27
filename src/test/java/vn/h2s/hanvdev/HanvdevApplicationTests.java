a730cc2060e328885cfa76aa16dc3e4e72df6564
HaHust <vanha.hust@gmail.com>
2022-07-27 23:40:38 +0700

    !

---
+++ src/test/java/vn/h2s/hanvdev/HanvdevApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.h2s.hanvdev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HanvdevApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
